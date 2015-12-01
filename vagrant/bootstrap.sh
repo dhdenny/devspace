030b63e8aa09b379563213d4d76ef284e188e198
Doug Denny <dhdenny@gmail.com>
2015-11-30 22:55:38 -0600

    discarded uneeded items from bootstrap

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -17,7 +17,7 @@ sudo apt-get install apache2 -y > /dev/null
 # PHP
 echo "Updating PHP repository"
 sudo apt-get install python-software-properties -y > /dev/null
-sudo add-apt-repository ppa:ondrej/php5-oldstable -y > /dev/null
+#sudo add-apt-repository ppa:ondrej/php5-oldstable -y > /dev/null
 sudo apt-get update > /dev/null
 sudo apt-get install -y php5 > /dev/null
 
@@ -66,18 +66,6 @@ sudo apt-get install git -y > /dev/null
 echo "Installing Composer"
 sudo curl -sS https://getcomposer.org/installer | php > /dev/null
 sudo mv composer.phar /usr/local/bin/composer > /dev/null
-
-# Installing Node
-echo "Installing Node"
-sudo curl -sL https://deb.nodesource.com/setup | sudo bash - > /dev/null
-sudo sudo apt-get install -y nodejs > /dev/null
-sudo npm install npm -g
-
-echo "Installing Bower"
-sudo npm install bower -g > /dev/null
-
-echo "Installing Gulp"
-sudo npm install Gulp -g > /dev/null
 
 
 echo "Finished provisioning."
